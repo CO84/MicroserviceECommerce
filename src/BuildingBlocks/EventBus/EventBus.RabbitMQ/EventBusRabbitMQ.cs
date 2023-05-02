7c59b7590fd12e56e61849a6e4ce51d0fbadc33f
CİHAN ÖZÇELİK <cihan84_ozcelik@hotmail.com>
2023-05-02 23:54:14 +0300

    Add project files.

---
+++ src/BuildingBlocks/EventBus/EventBus.RabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,33 @@
+﻿using EventBus.Base;
+using EventBus.Base.Events;
+
+namespace EventBus.RabbitMQ
+{
+    public class EventBusRabbitMQ : BaseEventBus
+    {
+        public EventBusRabbitMQ(EventBusConfig eventBusConfig, IServiceProvider serviceProvider) : base(eventBusConfig, serviceProvider)
+        {
+        }
+
+        public override void Publish(IntegrationEvent @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Subscribe<T, TH>()
+        {
+            var eventName = typeof(T).Name;
+            eventName = ProcessEventName(eventName);
+
+            if (!SubsManager.HasSubscriptionForEvent(eventName))
+            {
+
+            }
+        }
+
+        public override void UnSubscribe<T, TH>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
